a1e0c7154f1cb08747215de63b51e640156d9f7e
Koga Atsushi <koga.a@sky-co.jp>
2025-01-06 19:04:05 +0900

    テスト

--- test.bat
+++ test.bat
@@ -1,1 +1,19 @@
-test
+@echo off
+:: テストファイルのパスを設定
+set TEST_FILE=%~dp0test_file.txt
+
+:: テストファイルを作成
+echo This is a test file. > %TEST_FILE%
+
+:: テストファイルの内容を表示
+type %TEST_FILE%
+
+:: テストファイルの存在を確認
+if exist %TEST_FILE% (
+    echo Test file created successfully.
+) else (
+    echo Failed to create test file.
+)
+
+:: 終了
+exit
